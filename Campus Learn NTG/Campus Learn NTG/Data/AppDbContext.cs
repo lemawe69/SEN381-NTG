30bd6cfd8dbe3c4aed3e13c8cf748dd8202c5ec0
lemawe69 <brandonlemmer777@gmail.com>
2025-10-19 16:19:53 +0200

    Pivoting

--- Campus Learn NTG/Campus Learn NTG/Data/AppDbContext.cs
+++
@@ -1,9 +0,0 @@
-using Microsoft.EntityFrameworkCore;
-
-public class AppDbContext : DbContext
-{
-    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
-
-    // Add DbSet properties for your tables, e.g.:
-    // public DbSet<User> Users { get; set; }
-}
